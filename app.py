0b3a188a51a5626704485024aff4fae78e3a4189
Arpit Sharma <s.arpit9656@gmail.com>
2023-09-03 17:33:12 +0530

    bug fixes

--- app.py
+++ app.py
@@ -1,36 +1,35 @@
 import logging
+import os
 from flask import Flask
-import api
+from api import api
 import config
 from models import db
 
 logging.basicConfig(level=logging.DEBUG,
-                   format='[%(asctime)s]: {} %(levelname)s %(message)s'.format(os.getpid()),
-                   datefmt='%Y-%m-%d %H:%M:%S',
-                   handlers=[logging.StreamHandler()])
+                    format='[%(asctime)s]: {} %(levelname)s %(message)s'.format(os.getpid()),
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    handlers=[logging.StreamHandler()])
 
 logger = logging.getLogger()
 
 
 def create_app():
-   logger.info(f'Starting app in {config.APP_ENV} environment')
-   app = Flask(__name__)
-   app.config.from_object('config')
-   api.init_app(app)
-   # initialize SQLAlchemy
-   db.init_app(app)
+    logger.info(f'Starting app in {config.APP_ENV} environment')
+    app = Flask(__name__)
+    app.config.from_object('config')
+    api.init_app(app)
+    # initialize SQLAlchemy
+    db.init_app(app)
 
-   # define hello world page
+    # define hello world page
 
-   @app.route('/')
-   def hello_world():
-       return 'Hello, World!'
-   return app
+    @app.route('/')
+    def hello_world():
+        return 'Hello, World!'
+
+    return app
 
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(host='0.0.0.0', debug=True)</td>
-    </tr>
-    <tr>
-        <td>
+    app.run(host='0.0.0.0', debug=True)
